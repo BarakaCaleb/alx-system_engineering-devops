b6803f1e5a520f1002a22cde69de73d38215cf31
Breezekid <calebbaraka79@gmail.com>
2024-03-27 11:41:47 +0300

    Debbuged Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,18 @@
-# Using Puppet to install Flask from pip 3
-# Requirements 
-#            install Flask
-#            Must version 2.1.0
+# Ensure that pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
 
-exec {
-    'install_flask':
-    command => '/usr/bin/pip3 install flask==2.1.0',
-    path => ['/usr/bin/'],
-    unless => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0",
+# Install Flask version 2.1.0 using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+}
+
+# Ensure Flask is installed globally
+file { '/usr/local/bin/flask':
+  ensure => link,
+  target => '/usr/local/bin/flask',
+  require => Exec['install_flask'],
 }
