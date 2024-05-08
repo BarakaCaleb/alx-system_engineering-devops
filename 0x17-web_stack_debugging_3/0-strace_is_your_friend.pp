6ab769685b3f810b2f9c56941ea7a35cc9a315cc
Breezekid <calebbaraka79@gmail.com>
2024-05-08 23:44:19 +0300

    debugged

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
-# This fixes bad `phhp` extension to `php` in the wordpress file `wp-settings.php`
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
 exec { 'fix-wordpress':
-  command => 'sed -i s/phhp/php/g /var/www/html/wp-settings.php',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
-
